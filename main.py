83e9cf2cc1da62d13e1cba7e453625feb5d774ec
Tamim <86140595+Tamim1234567@users.noreply.github.com>
2023-07-29 15:50:54 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'your-secret-key'  # Replace with your own secret key
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template('index.html')
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected!')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected!')
+
+@socketio.on('message_from_client')
+def handle_message(data):
+    message = data['message']
+    print(f'Received message: {message}')
+    # Broadcast the message to all connected clients
+    emit('message_from_server', {'message': message}, broadcast=True)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    socketio.run(app, debug=True)
